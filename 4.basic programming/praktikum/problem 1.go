90137670bb44e4331fe4b548b1d99e5b65697725
indah lestari <muarabeliti55@gmail.com>
2022-02-26 15:09:09 +0700

    insialisasi problem 2.go

---
+++ 4.basic programming/praktikum/problem 1.go
@@ -0,0 +1,10 @@
+package main
+
+func main() {
+	// input
+	T := 20.0
+	r := 4.0
+
+	// kode disini
+
+}
---
+++ 4.basic programming/praktikum/problem 2.go
@@ -0,0 +1,11 @@
+package main
+
+func main() {
+	// input
+	var studentscore int = 80
+
+	// process: Your solution code here
+
+	// output
+	//Nilai A
+}
