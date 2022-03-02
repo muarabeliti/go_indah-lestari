862efa6e9e3cf69c4343575881c72e28fab0fa7d
indah lestari <muarabeliti55@gmail.com>
2022-03-02 16:50:42 +0700

    insialisasi problem5.go

--- 4.basic programming/praktikum/problem5.go
+++ 4.basic programming/praktikum/problem5.go
@@ -2,32 +2,31 @@ package main
 
 import "fmt"
 
-func palindrome (kata string) {
-	var hasilCek,hasilAkhir string
+func palindrome(kata string) {
+	var hasilCek, hasilAkhir string
 	var panjangHuruf int
 	panjangHuruf = len(kata)
 
-	for x :=0; x < int(panjang Huruf) ; x++ {
-		y := (panjangHuruf -1)-x
-		hasilCek = fmt.Sprintf("%c",kata[y])
+	for x := 0; x < int(panjangHuruf); x++ {
+		y := (panjangHuruf - 1) - x
+		hasilCek = fmt.Sprintf("%c", kata[y])
 
-	if hasilCek != fmt.Sprintf("%c", kata[x]){
-		hasilAkhir ="bukan palindrome"
-		break
-	}else {
-		hasilAkhir ="palindrome"
+		if hasilCek != fmt.Sprintf("%c", kata[x]) {
+			hasilAkhir = "bukan palindrome"
+			break
+		} else {
+			hasilAkhir = "palindrome"
+		}
+
 	}
-
-
-	}
-fmt.Println("kata",kata,"merupakan=",hasilAkhir)
+	fmt.Println("kata", kata, "merupakan=", hasilAkhir)
 
 }
 
-func main (){
-	palindrome ("civic")
-	palindrome ("katak")
-	palindrome ("kasur rusak")
-	palindrome ("mistar")
-	palindrome ("lion")
+func main() {
+	palindrome("civic")
+	palindrome("katak")
+	palindrome("kasur rusak")
+	palindrome("mistar")
+	palindrome("lion")
 }
